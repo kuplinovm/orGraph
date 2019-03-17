c84cf38459b8e5d7231ba1c940ffc0c1b9c02bdb
kuplinovm <kuplinov.m3@gmail.com>
2019-03-17 16:59:20 +0300

    Class done

--- src/class.kt
+++ src/class.kt
@@ -22,7 +22,49 @@ data class orGraph(val points: List<String>, val arcs: List<Pair<Pair<String, St
     }
 
     fun renamePoint(old: String, new: String) {
+        if (points.contains(old)) {
+            points.minus(old)
+            points.plus(new)
+            arcs.forEach { if (it.first.first == old) {
+                arcs.minus(it)
+                arcs.plus(Pair(Pair(new, it.first.second), it.second))
+            }
+            else if (it.first.second == old) {
+                arcs.minus(it)
+                arcs.plus(Pair(Pair(it.first.first, new), it.second))
+            }
+            }
+        }
+    }
 
+    fun changeWeight (point1: String, point2: String, oldWeight: Double, newWeight: Double) {
+        if (points.contains(point1) && points.contains(point2))
+            arcs.forEach { if (it == Pair(Pair(point1, point2), oldWeight)) {
+                arcs.minus(it)
+                arcs.plus(Pair(Pair(point1, point2), newWeight))
+                }
+            }
+        else throw Exception()
+    }
+
+    fun listOfOutgoingArcs (name: String): List<Pair<Pair<String, String>, Double>> {
+        val res = listOf(Pair(Pair("0", "0"), 0.0))
+        if (points.contains(name))
+            for (element in arcs)
+                if (element.first.first == name)
+                    res.plus(element)
+        else throw Exception()
+        return res.drop(0)
+    }
+
+    fun listOfIncomingArcs (name: String): List<Pair<Pair<String, String>, Double>> {
+        val res = listOf(Pair(Pair("0", "0"), 0.0))
+        if (points.contains(name))
+            for (element in arcs)
+                if (element.first.second == name)
+                    res.plus(element)
+        else throw Exception()
+        return res.drop(0)
     }
 }
 
